fc91569265f2ddfb6d63a1d326333e0d8ab36a0c
msohini <msohini1995@gmail.com>
2019-11-18 15:36:22 +0530

    Adding basic login service and error handling

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { AuthService, AuthResponseData } from './auth.service';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-auth',
@@ -24,20 +26,22 @@ export class AuthComponent {
 		const email = form.value.email;
 		const password = form.value.password;
 		this.isLoading = true;
+		let authObs: Observable<AuthResponseData>;
 		if (this.isLoginMode) {
+			authObs = this.authservice.login(email, password);
 		}
 		else {
-			this.authservice.signup(email, password).subscribe(resData => {
-				console.log(resData);
-				this.isLoading = false;
-			},
-				errorMessage => {
-					console.log(errorMessage);
-					this.error = errorMessage;
-					this.isLoading = false;
-				});
+			authObs = this.authservice.signup(email, password);
 		}
-		
+		authObs.subscribe(resData => {
+			console.log(resData);
+			this.isLoading = false;
+		},
+			errorMessage => {
+				console.log(errorMessage);
+				this.error = errorMessage;
+				this.isLoading = false;
+			});
 
 		form.reset();
 	}
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,19 +2,20 @@
 
 
 import { Injectable } from '@angular/core'
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
 import { map, tap, catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
 	kind: string;
 	idToken: string;
 	email: string;
 	refreshToken: string;
 	expiresIn: string;
 	localId: string;
+	registered?: boolean;
 
 }
 @Injectable({ providedIn: 'root' })
@@ -30,21 +31,44 @@ export class AuthService {
 				password: password,
 				returnSecureToken: true
 			}
-		).pipe(catchError(ErrorRes => {
-			let errorMessage = 'An unknown error occured!';
-			if (!ErrorRes.error || !ErrorRes.error.error) {
-				return throwError(errorMessage);
-			}
-			
-				switch (ErrorRes.error.error.message) {
-					case 'EMAIL_EXISTS': errorMessage = 'Email Already exists!!';
-				}
-		
-			return throwError(errorMessage);
-		}));
+		).pipe(catchError(this.handleError));
 
 	}
 
+
+	login(email: string, password: string) {
+
+
+		return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBoIqGCGxgEH3FbY3z2oaJNrUQlmiUDCEI',
+			{
+				email: email,
+				password: password,
+				returnSecureToken: true
+			}
+		).pipe(catchError(this.handleError));
+	}
+
+
+	private handleError(ErrorRes: HttpErrorResponse) {
+		let errorMessage = 'An unknown error occured!';
+		if (!ErrorRes.error || !ErrorRes.error.error) {
+			return throwError(errorMessage);
+		}
+
+		switch (ErrorRes.error.error.message) {
+			case 'EMAIL_EXISTS': errorMessage = 'Email Already exists!!';
+				break;
+			case 'EMAIL_NOT_FOUND': errorMessage = 'Email does not exist!!';
+				break;
+			case 'INVALID_PASSWORD': errorMessage = 'Wrong Password!!';
+				break;
+			case 'USER_DISABLED': errorMessage = 'User Disabled!!';
+				break;
+		}
+
+		return throwError(errorMessage);
+
+	}
 
 }
 
