511e13506bb2b1524a569bc3eea6f36d18258a65
msohini <msohini1995@gmail.com>
2019-11-21 15:47:32 +0530

    adding Auth Guard and complete authentication module

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,33 @@
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, UrlTree } from '@angular/router';
+
+import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Promise } from 'q';
+import { Observable } from 'rxjs';
+import { map, tap, take } from 'rxjs/operators';
+
+
+@Injectable({ providedIn: 'root' })
+export class authGuard implements CanActivate {
+	constructor(private authservice: AuthService, private router: Router) { }
+	canActivate
+		(route: ActivatedRouteSnapshot, router: RouterStateSnapshot)
+		: boolean | UrlTree | Promise<boolean | UrlTree> | Observable<boolean | UrlTree> | UrlTree {
+
+		return this.authservice.user.pipe(take(1),  map(user => {
+		const	isAuth = !!user;
+			if (isAuth) {
+				return false;
+			}
+			return this.router.createUrlTree(['/auth']);
+		}),
+			//tap((isAuth => {
+			//if (!isAuth) {
+			//	this.router.navigate(['/auth']);
+			//}
+		//}
+    //))
+
+		);
+	}
+}
